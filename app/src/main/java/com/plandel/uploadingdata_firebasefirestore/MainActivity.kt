aabd347437eedcbc310ef5c5d817f88ef8d9070c
kaioc <kaioplandel@gmail.com>
2022-05-03 12:13:49 -0300

    update

---
+++ app/src/main/java/com/plandel/uploadingdata_firebasefirestore/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.plandel.uploadingdata_firebasefirestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.plandel.uploadingdata_firebasefirestore.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    private val personCollectionRef = Firebase.firestore.collection("persons")
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener {
+            val firstName = binding.editFirstName.text.toString()
+            val lastName = binding.editLastName.text.toString()
+            val age = Integer.parseInt(binding.editAge.text.toString())
+
+            val person = Person(firstName,lastName,age)
+            savePerson(person)
+        }
+    }
+
+    private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            personCollectionRef.add(person).await()
+            withContext(Dispatchers.Main){
+                Toast.makeText(this@MainActivity, "Add Successfully", Toast.LENGTH_SHORT).show()
+            }
+        }catch (e: Exception){
+            withContext(Dispatchers.Main){
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/plandel/uploadingdata_firebasefirestore/Person.kt
@@ -0,0 +1,7 @@
+package com.plandel.uploadingdata_firebasefirestore
+
+data class Person(
+    var firstName: String,
+    var lastName: String,
+    var age: Int
+)
